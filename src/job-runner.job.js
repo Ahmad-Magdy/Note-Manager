31562fbf2c0e483d1f9e13273059ba0de830b0b9
Ahmad Magdy <ahmedmagdi@outlook.com>
2018-07-29 00:42:15 +0200

    implement cron job for notes

---
+++ src/job-runner.job.js
@@ -0,0 +1,15 @@
+import { CronJob } from 'cron';
+import mongoose from 'mongoose';
+import { noteOperations } from './implementation/index';
+import config from './config';
+
+mongoose.connect(config.dbCN).then(() => console.info('Connected to MongoDB'));
+
+const job = new CronJob('0 */5 * * * *', async () => {
+  console.log('You will see this message every s');
+  const notes = await noteOperations.getScheduledNotes();
+  notes.forEach((note) => {
+    console.log(note);
+    // You can run any function right here;
+  });
+}, null, true);
